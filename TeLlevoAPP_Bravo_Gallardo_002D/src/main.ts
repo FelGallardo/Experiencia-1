251bb4986697c5eeaa6d156ccf9f266b58656536
FelGallardo <128819160+FelGallardo@users.noreply.github.com>
2023-11-20 03:29:09 -0300

    Experiencia 3

--- TeLlevoAPP_Bravo_Gallardo_002D/src/main.ts
+++ TeLlevoAPP_Bravo_Gallardo_002D/src/main.ts
@@ -4,9 +4,21 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// Importa aquí la inicialización de Firebase
+import { initializeApp } from 'firebase/app';
+
+
+if(!navigator.geolocation){
+  alert('Aplicacion no soporta la Geolocalizacion');
+  throw new Error('Aplicacion no soporta la Geolocalizacion');
+}
+
 if (environment.production) {
   enableProdMode();
 }
 
+// Inicializa Firebase antes de iniciar la aplicación Angular
+initializeApp(environment.firebaseConfig);
+
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+  .catch(err => console.error(err));
