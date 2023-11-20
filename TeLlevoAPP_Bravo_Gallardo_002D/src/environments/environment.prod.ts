8dbef7f12438a5b5591f71817a465df344262034
FelGallardo <128819160+FelGallardo@users.noreply.github.com>
2023-11-20 03:30:26 -0300

    Experiencia 3

--- TeLlevoAPP_Bravo_Gallardo_002D/src/environments/environment.prod.ts
+++ TeLlevoAPP_Bravo_Gallardo_002D/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true, 
+  firebaseConfig : {
+    apiKey: "AIzaSyBrH-y--CVDHfowKT5cpxPl8eoFJFH68Yc",
+    authDomain: "tellevoapp-59f22.firebaseapp.com",
+    projectId: "tellevoapp-59f22",
+    storageBucket: "tellevoapp-59f22.appspot.com",
+    messagingSenderId: "324375557550",
+    appId: "1:324375557550:web:81ae03024e46aefc03843f",
+    measurementId: "G-BGTKVHNNJF"
+  },
+  
+  googleMapsApiKey:''
 };
--- TeLlevoAPP_Bravo_Gallardo_002D/src/environments/environment.ts
+++ TeLlevoAPP_Bravo_Gallardo_002D/src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBrH-y--CVDHfowKT5cpxPl8eoFJFH68Yc",
+    authDomain: "tellevoapp-59f22.firebaseapp.com",
+    projectId: "tellevoapp-59f22",
+    storageBucket: "tellevoapp-59f22.appspot.com",
+    messagingSenderId: "324375557550",
+    appId: "1:324375557550:web:81ae03024e46aefc03843f",
+    measurementId: "G-BGTKVHNNJF"
+  },
+  googleMapsApiKey:''
+
 };
 
 /*
